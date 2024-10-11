2430e2b56d8c528279776cd130d9b2a86ba9d92f
Bosco <yuliayzi.lis2@gmail.com>
2024-10-11 14:38:40 +0300

    Ваше повідомлення про коміт

---
+++ danulko_sample_project/calculator_app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+def calculator(request):
+    if request.method == 'POST':
+        num1 = float(request.POST.get('num1'))
+        num2 = float(request.POST.get('num2'))
+        operation = request.POST.get('operation')
+
+        if operation == 'add':
+            result = num1 + num2
+        elif operation == 'subtract':
+            result = num1 - num2
+        elif operation == 'multiply':
+            result = num1 * num2
+        elif operation == 'divide':
+            result = num1 / num2
+        else:
+            result = 'Invalid operation'
+
+        return JsonResponse({'result': result})
+
+    return render(request, 'calculator.html')
---
+++ danulko_sample_project/guess_game_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GuessGameAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'guess_game_app'
